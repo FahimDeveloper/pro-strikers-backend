8601486a302f4e9803f173192182707f1b58570a
FahimDeveloper <developerfahim246@gmail.com>
2024-06-04 15:36:41 +0600

    fix vercel issue

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,25 @@ import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
+// const corsConfig = {
+//   origin: ['http://localhost:5173', 'https://prostrikers-admin.netlify.app'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+// };
+// app.use(express.json());
+// app.use(cookieParser());
+// app.options('', cors(corsConfig));
+// app.use(cors(corsConfig));
+
 //parser
-const corsConfig = {
-  origin: ['http://localhost:5173', 'https://prostrikers-admin.netlify.app'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-};
 app.use(express.json());
 app.use(cookieParser());
-app.options('', cors(corsConfig));
-app.use(cors(corsConfig));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://prostrikers-admin.netlify.app'],
+    credentials: true,
+  }),
+);
 
 app.use('/api/v1', router);
 
