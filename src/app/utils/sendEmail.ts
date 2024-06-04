ea81a76c849d6de967a7f4f2de2ae80451008c10
FahimDeveloper <developerfahim246@gmail.com>
2024-06-04 17:01:19 +0600

    try to fix vercel issue

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,36 +1,37 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import config from '../config';
 
 type TProp = { email: string; link?: string; code?: number };
 
 export const sendEmail = async ({ email, link, code }: TProp) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    service: 'gmail',
-    port: 587,
-    secure: true,
-    auth: {
-      user: config.node_mailer_mail,
-      pass: config.node_mailer_mail_pass,
-    },
-  });
+  console.log('hello');
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   service: 'gmail',
+  //   port: 587,
+  //   secure: true,
+  //   auth: {
+  //     user: config.node_mailer_mail,
+  //     pass: config.node_mailer_mail_pass,
+  //   },
+  // });
 
-  if (link) {
-    await transporter.sendMail({
-      from: config.node_mailer_mail,
-      to: email,
-      subject: 'Password Reset Link',
-      html: `Reset your password within 15 minutes <a href="${link}">${link}</a>`,
-    });
-  }
+  // if (link) {
+  //   await transporter.sendMail({
+  //     from: config.node_mailer_mail,
+  //     to: email,
+  //     subject: 'Password Reset Link',
+  //     html: `Reset your password within 15 minutes <a href="${link}">${link}</a>`,
+  //   });
+  // }
 
-  if (code) {
-    await transporter.sendMail({
-      from: config.node_mailer_mail,
-      to: email,
-      subject: 'Reset Code',
-      text: `Use this code ${code} to reset your password`,
-    });
-  }
+  // if (code) {
+  //   await transporter.sendMail({
+  //     from: config.node_mailer_mail,
+  //     to: email,
+  //     subject: 'Reset Code',
+  //     text: `Use this code ${code} to reset your password`,
+  //   });
+  // }
   return;
 };
