11d2f3e37d15b130dfdd1255d1a87f4f284afd73
FahimDeveloper <developerfahim246@gmail.com>
2024-06-04 17:13:51 +0600

    try to fix server issue

--- src/app.ts
+++ src/app.ts
@@ -15,16 +15,14 @@ app.use(cookieParser());
 //   credentials: true,
 //   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
 // };
-// app.use(express.json());
-// app.use(cookieParser());
 // app.options('', cors(corsConfig));
-// app.use(cors(corsConfig));
-app.use(
-  cors({
-    origin: ['http://localhost:5173'],
-    credentials: true,
-  }),
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true,
+//   }),
+// );
 
 app.use('/api/v1', router);
 
