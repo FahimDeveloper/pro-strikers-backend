67d6ef801caf1a5c800c1ce033de85b7228d33ee
FahimDeveloper <developerfahim246@gmail.com>
2024-09-15 13:24:49 +0600

    try to host in vercel

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import config from './app/config';
+import { Server } from 'http';
 
 const port = process.env.PORT || config.port;
-
-// let server: Server;
+let isConnected = false;
+let server: Server;
 
 async function dbConnection() {
+  if (isConnected) {
+    return;
+  }
   const url =
     config.node_env === 'production'
       ? config.database_url
       : config.database_local_url;
   try {
     await mongoose.connect(url as string);
+    isConnected = true;
     console.log(`app server listening on ${port}`);
   } catch (err) {
     console.log(err);
@@ -20,24 +25,24 @@ async function dbConnection() {
 }
 dbConnection();
 
+process.on('uncaughtException', () => {
+  console.log('uncaughtException detected, server closed');
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('unhandledRejection', () => {
+  console.log('unhandledRejection detected, server closed');
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
 export default app;
-
-// process.on('uncaughtException', () => {
-//   console.log('uncaughtException detected, server closed');
-//   if (server) {
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   }
-//   process.exit(1);
-// });
-
-// process.on('unhandledRejection', () => {
-//   console.log('unhandledRejection detected, server closed');
-//   if (server) {
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   }
-//   process.exit(1);
-// });
