2a71a23a6473a45fe9409878ae07933b6e75a919
FahimDeveloper <developerfahim246@gmail.com>
2024-09-15 14:06:39 +0600

    fix an issue

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,16 @@ import config from './app/config';
 import { Server } from 'http';
 
 const port = process.env.PORT || config.port;
-let isConnected = false;
+
 let server: Server;
 
 async function dbConnection() {
-  if (isConnected) {
-    return;
-  }
   const url =
     config.node_env === 'production'
       ? config.database_url
       : config.database_local_url;
   try {
     await mongoose.connect(url as string);
-    isConnected = true;
     console.log(`app server listening on ${port}`);
   } catch (err) {
     console.log(err);
@@ -44,5 +40,3 @@ process.on('unhandledRejection', () => {
   }
   process.exit(1);
 });
-
-export default app;
