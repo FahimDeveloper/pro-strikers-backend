630daba30d840b91a12b41922b04c40431e701ee
FahimDeveloper <developerfahim246@gmail.com>
2024-11-02 22:50:29 +0600

    redirect to the all origin access

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,28 @@ import notFound from './app/middlewares/notFound';
 
 export const app: Application = express();
 
+// app.use(
+//   cors({
+//     origin: ['https://prostrikers.com', 'https://admin.prostrikers.com'],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+//   }),
+// );
+// app.options('*', cors());
+
 app.use(
   cors({
-    origin: ['https://prostrikers.com', 'https://admin.prostrikers.com'],
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      callback(null, true);
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   }),
 );
 
-app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1', router);
